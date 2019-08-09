9380d448c77003e18b4553de41c376cb4ef8aab9
Jannis R <mail@jannisr.de>
2019-08-09 23:05:18 +0200

    about page

--- index.js
+++ index.js
@@ -3,15 +3,41 @@
 const {createServer} = require('http')
 const feed = require('./api/feed')
 
+const aboutPage = (host) => `
+	<!DOCTYPE html>
+	<head>
+	<meta charset="utf-8">
+	<title>Critical Mass Berlin calendar feed</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+	<h1>Critical Mass Berlin calendar feed</h1>
+	<p>
+		<a href="webcal://${host}/api/feed">subscribe to this calendar</a>
+	</p><p>
+		If the link above doesn't open your calendar, copy the link below and create a subscription in your calendar app:
+		<br>
+		<a href="https://${host}/api/feed">
+			<code style="display:block">https://${host}/api/feed</code>
+		</a>
+	</p>
+	</body>
+	</html>
+`
+
 const server = createServer((req, res) => {
-	const path = new URL(req.url, 'http://example.org').pathname.slice(1)
-	if (path !== 'critical-mass-berlin.ics') {
+	const path = new URL(req.url, 'http://example.org').pathname
+
+	if (path === '/') {
+		const {host} = req.headers
+		res.writeHead(200, 'ok', {'content-type': 'text/html'})
+		res.end(aboutPage(host))
+	} else if (path === '/api/feed') {
+		feed(req, res)
+	} else {
 		res.writeHead(404, 'not found')
 		res.end('not found')
-		return
 	}
-
-	feed(req, res)
 })
 
 const port = parseInt(process.env.PORT || 3000)
